61648299225995084de62b22d72ac9005bdd2213
Jason <sanchezjason@me.com>
2024-03-05 16:15:05 -0700

    swiftui/ML_face_highlight- Project setup, FaceHighlightView

---
+++ FaceHighlightRecognition/FaceHighlightRecognition/FaceHighlightRecognitionApp.swift
@@ -0,0 +1,17 @@
+//
+//  FaceHighlightRecognitionApp.swift
+//  FaceHighlightRecognition
+//
+//  Created by Cause  I'm Electric on 3/5/24.
+//
+
+import SwiftUI
+
+@main
+struct FaceHighlightRecognitionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            FaceHighlightView()
+        }
+    }
+}
---
+++ FaceHighlightRecognition/FaceHighlightRecognition/FaceHighlightView.swift
@@ -0,0 +1,18 @@
+//
+//  FaceHighlightView.swift
+//  FaceHighlightRecognition
+//
+//  Created by Cause  I'm Electric on 3/5/24.
+//
+
+import SwiftUI
+
+struct FaceHighlightView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FaceHighlightView()
+}
